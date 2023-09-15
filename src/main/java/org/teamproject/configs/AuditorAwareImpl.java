83c385e6092f67bb64390842d5bbddf8818bbf33
yonggyo1125 <yonggyo00@kakao.com>
2023-09-15 19:36:27 +0900

    초기 구현

---
+++ src/main/java/org/teamproject/configs/AuditorAwareImpl.java
@@ -0,0 +1,15 @@
+package org.teamproject.configs;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        return Optional.empty();
+    }
+}
--- src/main/java/org/teamproject/entities/Member.java
+++ src/main/java/org/teamproject/entities/Member.java
@@ -1,4 +1,40 @@
 package org.teamproject.entities;
 
-public class Member {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.teamproject.commons.constants.Role;
+
+@Entity
+@Data @Builder
+@AllArgsConstructor @NoArgsConstructor
+public class Member extends BaseEntity {
+    @Id @GeneratedValue
+    private Long userNo;
+
+    @Column(length=100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length=40, nullable = false)
+    private String userNm;
+
+    @Column(length=65, nullable = false)
+    private String userPw;
+
+    @Column(length=11, nullable = false)
+    private String mobile;
+
+    @Column(length=10, nullable = false)
+    private String zonecode;
+
+    @Column(length=100, nullable = false)
+    private String address;
+
+    @Column(length=100)
+    private String addressSub;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
