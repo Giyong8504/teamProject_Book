962508d6b64b323fc49fafbb60cf8830ebe92d26
yonggyo1125 <yonggyo00@kakao.com>
2023-09-15 19:51:22 +0900

    초기 구현

--- src/main/java/org/teamproject/models/member/UserInfoService.java
+++ src/main/java/org/teamproject/models/member/UserInfoService.java
@@ -1,14 +1,44 @@
 package org.teamproject.models.member;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.teamproject.entities.Member;
+import org.teamproject.repositories.MemberRepository;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserInfoService implements UserDetailsService {
+
+    private final MemberRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        Member member = repository.findByEmail(username);
+        if (member == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        List<GrantedAuthority> authorities = Arrays.asList(
+          new SimpleGrantedAuthority(member.getRole().name())
+        );
+
+        return UserInfo.builder()
+                .userNo(member.getUserNo())
+                .email(member.getEmail())
+                .userPw(member.getUserPw())
+                .userNm(member.getUserNm())
+                .mobile(member.getMobile())
+                .role(member.getRole())
+                .authorities(authorities)
+                .build();
     }
 }
