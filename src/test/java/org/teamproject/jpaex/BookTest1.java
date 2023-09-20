350343d365224319400431d6f536685c2ac53e09
Giyong8504 <kky5163@naver.com>
2023-09-20 20:56:25 +0900

    Autowired 애노테이션 임포트 제거

--- src/test/java/org/teamproject/jpaex/BookTest1.java
+++ src/test/java/org/teamproject/jpaex/BookTest1.java
@@ -3,7 +3,6 @@ package org.teamproject.jpaex;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
