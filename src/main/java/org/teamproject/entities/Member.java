02664b5f27340c566e52ed457ddca8e38f20a2c2
yeochan7101 <sun09821@naver.com>
2023-09-22 01:56:59 +0900

    정보 수정 기능 작업중

--- src/main/java/org/teamproject/entities/Member.java
+++ src/main/java/org/teamproject/entities/Member.java
@@ -5,39 +5,59 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.teamproject.commons.constants.Role;
 
 @Entity
 @Data @Builder
 @AllArgsConstructor @NoArgsConstructor
 public class Member extends BaseEntity {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long userNo;
 
-    @Column(length=100, nullable = false, unique = true)
+    @Column(length = 100, nullable = false, unique = true)
     private String email;
 
-    @Column(length=40, nullable = false)
+    @Column(length = 40, nullable = false)
     private String userNm;
 
-    @Column(length =100, nullable = false)
+    @Column(length = 100, nullable = false)
     private String age;
 
-    @Column(length=65, nullable = false)
+    @Column(length = 65, nullable = false)
     private String userPw;
 
-    @Column(length=11, nullable = false)
+    @Column(length = 11, nullable = false)
     private String mobile;
 
-    @Column(length=10, nullable = false)
+    @Column(length = 10, nullable = false)
     private String zonecode;
 
-    @Column(length=100, nullable = false)
+    @Column(length = 100, nullable = false)
     private String address;
 
-    @Column(length=100)
+    @Column(length = 100)
     private String addressSub;
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    //== 정보 수정 ==//
+    public void updatePassword(PasswordEncoder passwordEncoder, String password){
+        this.userPw = passwordEncoder.encode(password);
+    }
+
+    public void updateName(String name){
+        this.userNm = name;
+    }
+
+    public void updateAge(String age){
+        this.age = age;
+    }
+
 }
+
+
+
+
---
+++ src/test/java/org/teamproject/repositories/MemberRepositoryTest.java
@@ -0,0 +1,8 @@
+package org.teamproject.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MemberRepositoryTest {
+
+}
