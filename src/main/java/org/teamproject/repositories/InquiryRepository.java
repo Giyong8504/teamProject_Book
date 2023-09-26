c7927fea1bf7e5cb7146bf3a6e7f73ffd9a03de4
github SungWoo0121 <github sayu7189@naver.com>
2023-09-26 11:45:41 +0900

    게시판 관리를 위한 브랜치 생성 및 문의/리뷰 side 삭제

--- src/main/java/org/teamproject/repositories/InquiryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.teamproject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.teamproject.entities.InquiryBoard;
-
-@Repository
-public interface InquiryRepository extends JpaRepository<InquiryBoard, Long> {
-}
