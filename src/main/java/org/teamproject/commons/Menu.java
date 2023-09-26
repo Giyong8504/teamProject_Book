a4b43a8369704fcfe1152b26dae1267700fd95bd
github SungWoo0121 <github sayu7189@naver.com>
2023-09-26 13:36:51 +0900

    게시판 관리 틀 작성 및 게시판 등록/수정 목록 구성

--- src/main/java/org/teamproject/commons/Menu.java
+++ src/main/java/org/teamproject/commons/Menu.java
@@ -23,6 +23,14 @@ public class Menu {
             menus.add(new MenuDetail("add", "상품 등록", "/admin/product/add"));
             menus.add(new MenuDetail("category", "상품 분류", "/admin/product/category"));
         }
+
+        // 게시판 하위 메뉴
+        if (code.equals("board")){
+            menus.add(new MenuDetail("board", "게시판 목록", "/admin/board"));
+            menus.add(new MenuDetail("register", "게시판 등록/수정", "/admin/board/register"));
+            menus.add(new MenuDetail("posts", "게시글 관리", "/admin/board/posts"));
+        }
+
         return menus;
     }
 
