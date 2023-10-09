7e602aae00a0a5397dd71b72db2630b52954afb7
Giyong8504 <kky5163@naver.com>
2023-10-09 12:52:59 +0900

    book,orders status 추가

--- src/main/java/org/teamproject/commons/constants/BookStatus.java
+++ src/main/java/org/teamproject/commons/constants/BookStatus.java
@@ -1,9 +1,29 @@
 package org.teamproject.commons.constants;
 
+import org.teamproject.commons.Utils;
+
+import java.util.Arrays;
+import java.util.List;
+
 public enum BookStatus {
-    ON_SALE,
+    ON_SALE, // 판매중
+    STOP, // 판매중지
 
-    READY,
+    READY, // 상품 준비중
 
-    OUT_OF_STOCK
+    OUT_OF_STOCK; // 품절
+
+
+    public String getString() {
+        return Utils.getMessage("BookStatus." + name(), "common");
+    }
+
+    public static List<String[]> getList() {
+        return Arrays.asList(
+                new String[]{ON_SALE.name(),ON_SALE.getString()},
+                new String[]{STOP.name(),STOP.getString()},
+                new String[]{READY.name(),READY.getString()},
+                new String[]{OUT_OF_STOCK.name(),OUT_OF_STOCK.getString()}
+        );
+    }
 }
--- src/main/java/org/teamproject/repositories/BooksRepository.java
+++ src/main/java/org/teamproject/repositories/BooksRepository.java
@@ -1,7 +1,8 @@
 package org.teamproject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.teamproject.entities.Books;
 
-public interface BooksRepository extends JpaRepository<Books, Long> {
+public interface BooksRepository extends JpaRepository<Books, Long>, QuerydslPredicateExecutor<Books> {
 }
