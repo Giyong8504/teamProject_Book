ee9d212fa36692f85d5881b3d11e4e29a26c16f5
yeochan7101 <sun09821@naver.com>
2023-10-11 18:59:17 +0900

    아이디, 비밀번호 찾기 폼

--- src/main/java/org/teamproject/controllers/member/MemberController.java
+++ src/main/java/org/teamproject/controllers/member/MemberController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,7 +14,6 @@ import org.teamproject.commons.Utils;
 import org.teamproject.entities.Member;
 import org.teamproject.models.member.UserInfoService;
 import org.teamproject.models.member.UserSaveService;
-import org.teamproject.repositories.MemberRepository;
 
 import java.security.SecureRandom;
 import java.util.Optional;
